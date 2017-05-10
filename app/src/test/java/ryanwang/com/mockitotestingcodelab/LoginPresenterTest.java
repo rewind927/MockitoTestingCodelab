71eae5a06bc5a718e323dde2f6776e7249003d47
RyanWang <rewind927@gmail.com>
2017-05-10 16:23:23 +0800

    Add inOrder verification and static mock setup in test.

--- app/src/test/java/ryanwang/com/mockitotestingcodelab/LoginPresenterTest.java
+++ app/src/test/java/ryanwang/com/mockitotestingcodelab/LoginPresenterTest.java
@@ -4,17 +4,19 @@ import org.junit.Before;
 import org.junit.Test;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Captor;
+import org.mockito.InOrder;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
-
 
 public class LoginPresenterTest {
 
@@ -42,13 +44,23 @@ public class LoginPresenterTest {
 
 	@Test
 	public void login_success_showSuccessMessage() {
+		LoginView loginView = mock(LoginView.class);
+		LoginRepositoryImpl loginRepository = mock(LoginRepositoryImpl.class);
+		NetworkManager networkManager = mock(NetworkManager.class);
+		LoginPresenter loginPresenter = new LoginPresenterImpl(loginView,loginRepository,networkManager);
 		when(networkManager.isNetworkAvailable()).thenReturn(true);
+		//Actually, you don't verify it in this situation, it only for demonstrates stub function.
+//		assertTrue(networkManager.isNetworkAvailable());
 		loginPresenter.login("hihi@abc.com", "hi");
+		ArgumentCaptor<LoginRepository.Callback>callbackCaptor=ArgumentCaptor.forClass(LoginRepository.Callback.class);
 		verify(loginRepository).login(eq("hihi@abc.com"), anyString(), callbackCaptor.capture());
 		callbackCaptor.getValue().onSuccess();
 		verify(loginView, times(1)).showLoading();
 		verify(loginView, times(1)).hideLoading();
 		verify(loginView).showSuccessMessage(anyString());
+
+		// verifyNoMoreInteractions can be useful in some cases, but shouldn't be overused by using on all mocks in every test
+//		verifyNoMoreInteractions(networkManager);
 	}
 
 	@Test
@@ -57,9 +69,14 @@ public class LoginPresenterTest {
 		loginPresenter.login("hihi@cba.com", "hi");
 		verify(loginRepository).login(anyString(), anyString(), callbackCaptor.capture());
 		callbackCaptor.getValue().onFail();
-		verify(loginView, times(1)).showLoading();
-		verify(loginView, times(1)).hideLoading();
-		verify(loginView).showFailed(anyString());
+
+		//Mockito enables you to verify if interactions with a mock were performed in a given order using the InOrder API.
+		// It is possible to create a group of mocks and verify the call order of all calls within that group.
+		//
+		InOrder inOrder = Mockito.inOrder(loginView);
+		inOrder.verify(loginView, times(1)).showLoading();
+		inOrder.verify(loginView, times(1)).hideLoading();
+		inOrder.verify(loginView).showFailed(anyString());
 	}
 
 	@Test
